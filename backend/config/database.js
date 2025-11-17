235356349aae935677167261155d61998e15985b
subhash-kumar-nigam <nigamsubhash439@gmail.com>
2025-11-17 12:53:57 +0530

    fix mysql connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,  
+  host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
@@ -12,7 +12,7 @@ const connection = mysql.createConnection({
 
 connection.connect(err => {
   if (err) {
-    console.log("❌ MySQL Connection Error:", err);
+    console.error("❌ MySQL Connection Error:", err);
   } else {
     console.log("✅ Connected to Railway MySQL!");
   }
